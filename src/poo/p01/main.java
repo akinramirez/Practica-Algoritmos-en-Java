1f8364772b93e50fdf2413d4657a4b3a5ceaefd0
akinRamirez <akin.ramirez@hotmail.com>
2020-02-02 18:32:28 -0600

    Realizando Ejercicios con Programacion Orientada a Objetos, tambien se realizo un pequeno proyecto muy integrador, en el cual se aplica todos los conceptos de POO

---
+++ src/poo/p01/main.java
@@ -0,0 +1,23 @@
+package poo.p01;
+
+import javax.swing.JOptionPane;
+
+public class main {
+
+  public static void main(String[] args) {
+    Cuadrilatero c1;
+    float lado1, lado2;
+
+    lado1 = Float.parseFloat(JOptionPane.showInputDialog("Digite el lado1: "));
+    lado2 = Float.parseFloat(JOptionPane.showInputDialog("Digite el lado2: "));
+
+    if (lado1 == lado2) { //Es un cuadrado
+      c1 = new Cuadrilatero(lado1);
+    } else { //Es otro cuadrilatero
+      c1 = new Cuadrilatero(lado1, lado2);
+    }
+
+    System.out.println("El perimetro del cuadrilatero es: " + c1.getPerimetro());
+    System.out.println("El area del cuadrilatero es: " + c1.getArea());
+  }
+}
